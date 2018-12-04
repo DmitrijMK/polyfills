16486d17cfbb90342866012eb08b6b974464fdc5
dmitrijmk <dmitrijmk@gmail.com>
2018-12-04 09:18:04 +0200

    add 'new'

---
+++ new.js
@@ -0,0 +1,18 @@
+// new=========================
+function myNew(func) {
+    const that = Object.create(func.prototype);
+    func.apply(that);
+    return that;
+}
+
+function F() {
+    this.a = 10;
+}
+
+F.prototype.foo = function foo() {
+    return this.a;
+};
+
+const a = myNew(F);
+console.log(a); // { a: 10, __proto__: { foo, constructor } }
+console.log(a.foo()); // 10
